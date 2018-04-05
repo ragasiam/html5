503d20e901f8598238c84ba2ab0ef226c4de57b2
RAGASIAM <34998996+ragasiam@users.noreply.github.com>
2018-04-05 16:01:42 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var audio = new Audio("http://109.74.196.48:8251/ragasiam");
+
+$('#play-pause-button').on("click",function(){
+  if($(this).hasClass('fa-play'))
+   {
+     $(this).removeClass('fa-play');
+     $(this).addClass('fa-pause');
+     audio.play();
+   }
+  else
+   {
+     $(this).removeClass('fa-pause');
+     $(this).addClass('fa-play');
+     audio.pause();
+   }
+});
+
+audio.onended = function() {
+     $("#play-pause-button").removeClass('fa-pause');
+     $("#play-pause-button").addClass('fa-play');
+};
